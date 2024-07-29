74a28e96bcadddf937ec79994889374367084de9
Subham <chromaticshubham26@gmail.com>
2024-07-29 10:52:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className="grid grid-cols-3">
+        <div className="bg-red-500">Hi</div>
+        <div className="bg-yellow-500">Hi</div>
+        <div className="bg-green-500">Hi</div>
+        <div className="bg-pink-500">Hi</div>
+        <div className="bg-orange-500">Hi</div>
+        <div className="bg-lime-500">Hi</div>
+      </div>
+    </>
+  )
+}
+
+export default App
